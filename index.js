af2ae3837f4db2585e199a17d3ca29cb3d026098
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-08-04 18:39:19 +0200

    Add most basic koa app with two routes

---
+++ index.js
@@ -0,0 +1,82 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const dedent = require('dedent');
+const router = require('./lib/router');
+
+const app = new Koa();
+
+/**
+ * Serve static files
+ * TODO: Either implement HTTP/2 Push or check for dev environment
+ */
+
+app.use(static('public'));
+
+/**
+ * Wrap up anything that has been gathered and determin proper data type
+ */
+
+app.use(async (ctx, next) => {
+
+  /**
+   * Let all other middleware have a go at the request before acting on it
+   */
+
+  await next();
+
+  if (ctx.accepts('html')) {
+
+    /**
+     * Wrap up HTML response in a document
+     */
+
+    ctx.type = 'text/html';
+    ctx.body = dedent`
+      <!doctype html>
+      <html lang="en">
+      <head>
+        <title>The Global Goals</title>
+        <link rel="stylesheet" href="/index.css" />
+      </head>
+      <body>
+        ${ ctx.body }
+        ${ ctx.params.referrer ? `
+          <br />
+          Referer id: ${ ctx.params.referrer.split(/^ref-/)[1] }
+        ` : '' }
+      </body>
+      </html>
+    `;
+  } else if (ctx.accepts('json')) {
+
+    /**
+     * Send the raw state, whateever that may be
+     */
+
+    ctx.type = 'application/json';
+    ctx.body = JSON.stringify(ctx.state);
+  } else {
+
+    /**
+     * We only support HTML and JSON
+     */
+
+    ctx.status = 406;
+  }
+});
+
+/**
+ * Hook up all em' routes
+ */
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+/**
+ * Lift off
+ */
+
+app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.info(`🚀  Server listening at localhost:${ process.env.PORT }`);
+});
