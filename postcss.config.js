89e751112f0f9758c8ddef417e7adba2cab65682
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-09-12 11:10:01 +0200

    Add service worker

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,10 @@ module.exports = (ctx) => ({
     require('postcss-custom-media')(),
     require('postcss-selector-matches')(),
     require('postcss-custom-properties')(),
-    require('postcss-url')([{filter: '**/*.woff', url: 'inline'}, { url: 'copy', useHash: true }]),
+    require('postcss-url')([
+      {filter: '**/*.woff', url: 'inline'},
+      { url: 'copy', useHash: true }
+    ]),
     require('postcss-flexbugs-fixes')(),
     require('autoprefixer')({browsers: [ 'last 2 versions', 'ie >= 9', 'Firefox ESR' ]}),
     require('postcss-csso')()
