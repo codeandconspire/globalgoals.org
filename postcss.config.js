a9b97f9714db18e47ae98bc753787284c6762f93
Anton Trollbäck <anton@trollback.se>
2017-09-12 09:41:23 +0200

    Fix some stuff for IE9 and Firefox

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ module.exports = (ctx) => ({
     require('postcss-custom-properties')(),
     require('postcss-url')([{filter: '**/*.woff', url: 'inline'}, { url: 'copy', useHash: true }]),
     require('postcss-flexbugs-fixes')(),
-    require('autoprefixer')(),
+    require('autoprefixer')({browsers: [ 'last 2 versions', 'ie >= 9', 'Firefox ESR' ]}),
     require('postcss-csso')()
   ]
 });
