187e871aa2d3fcd40f29be514dfae3a4e10d957d
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-09-18 09:21:37 +0200

    Add postcss color function plugin to build

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,10 @@ module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: [
     require('postcss-import')(),
-    require('postcss-custom-media')(),
-    require('postcss-selector-matches')(),
     require('postcss-custom-properties')(),
+    require('postcss-custom-media')(),
+    require('postcss-color-function')(),
+    require('postcss-selector-matches')(),
     require('postcss-url')([
       {filter: '**/*.woff', url: 'inline'},
       { url: 'copy', useHash: true }
